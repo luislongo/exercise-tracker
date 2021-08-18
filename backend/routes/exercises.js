bf97fa747a9916ac5bb96b49cad0b48a70dc73ba
luislongo <luis.longof@gmail.com>
2021-08-18 17:04:41 -0300

    Added exercise delete button

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -34,7 +34,7 @@ router.route('/:id').get((req, res) => {
 })
 
 router.route('/:id').delete((req, res) => {
-    Exercise.findOneAndDelete(req.params.id)
+    Exercise.findByIdAndDelete(req.params.id)
         .then(() => res.json('Exercise deleted!'))
         .catch((err) => res.status(400).json('Error: '+ err)) 
 })
